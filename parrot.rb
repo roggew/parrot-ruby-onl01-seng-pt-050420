ef199a505c5ec138d1cc19cd1315f97b1e7e4dde
William Rogge <williamroggeiv@gmail.com>
2020-05-06 21:25:49 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(sound = "Squawk!")
-  puts sound
-  print (sound)
+def parrot(phrase = "Squawk!")
+  print phrase
 end
